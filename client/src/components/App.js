285fdf976bd1b71478519c4a38a7ab1f66b05782
bellom <bellomsean@gmail.com>
2019-12-02 16:37:25 +0100

    added DoctorList.js component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,36 +1,18 @@
 import React from "react";
 import "./App.css";
 import LandingPage from './LandingPage'
+import DoctorList from './DoctorList'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 class App extends React.Component {
-  // state = {
-  //   doctors: []
-  // };
-
-  // componentDidMount() {
-  //   fetch("/api/doctors")
-  //     .then(response => response.json())
-  //     .then(json => this.setState({ doctors: json }))
-  //     .catch(error => console.log(error));
-  // }
-
-  // renderDoctors() {
-  //   return this.state.doctors.map(doctor => (
-  //     <div key={doctor.id}>
-  //       <h2>{doctor.name}</h2>
-  //       <h3>{doctor.education}</h3>
-  //       <h4>{doctor.speciality}</h4>
-  //     </div>
-  //   )); 
-  // }
-
   render() {
     return (
       <Router>
         <Switch>
           <Route path='/' exact component={LandingPage} />
+          <Route path='/doctorlist' exact component={DoctorList} />
+
         </Switch>
       </Router>
     );
---
+++ client/src/components/DoctorList.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./App.css";
+
+class DoctorList extends React.Component {
+  
+  state = {
+    doctors: []
+  };
+
+  componentDidMount() {
+    fetch("/api/doctors")
+      .then(response => response.json())
+      .then(json => this.setState({ doctors: json }))
+      .catch(error => console.log(error));
+  }
+
+  renderDoctors() {
+    return this.state.doctors.map(doctor => (
+      <div key={doctor.id}>
+        <h2>{doctor.name}</h2>
+        <h3>{doctor.education}</h3>
+        <h4>{doctor.speciality}</h4>
+      </div>
+    )); 
+  }
+  
+  render() {
+  return (
+    <div className="App">
+      <div className="headTitle">
+        <span>&#60;</span> Doctors <span>&#x25bc;</span>
+      </div>
+      <header className="App-boy">
+        <div className="body">
+          <i className="material-icons input settings">settings_application</i>
+        </div>
+        <div>
+          <h3>List of Doctors from search</h3>
+          <h4>Filter bY Name: <input type='text'/></h4>
+          <div className="doctor">
+            <div>{this.renderDoctors()}</div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+  };
+};
+
+export default DoctorList;
