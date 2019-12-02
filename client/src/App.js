57e9f629e22e1661ce160f442c2785cc93f8b4c4
bellom <bellomsean@gmail.com>
2019-12-02 13:26:05 +0100

    created user model

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    doctors: []
+  };
+
+  componentDidMount() {
+    fetch("/api/doctors")
+      .then(response => response.json())
+      .then(json => this.setState({ doctors: json }))
+      .catch(error => console.log(error));
+  }
+
+  renderMovies() {
+    return this.state.doctors.map(doctor => (
+      <div key={doctor.id}>
+        <h2>{doctor.name}</h2>
+        <h3>{doctor.education}</h3>
+        <h4>{doctor.speciality}</h4>
+      </div>
+    ));
+  }
+
+  render() {
+    return <div className="App">{this.renderMovies()}</div>;
+  }
+}
+
+export default App;
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_145233) do
+ActiveRecord::Schema.define(version: 2019_12_02_120106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2019_11_29_145233) do
     t.string "name"
     t.string "education"
     t.string "speciality"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
