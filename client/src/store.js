b261c6c3147912153eec3e1a22faf455b87d8d0a
bellom <bellomsean@gmail.com>
2019-12-06 06:56:51 +0100

    added redux-dev-tools

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import asyncReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
-const store = createStore(asyncReducer, applyMiddleware(thunk));
+
+const store = createStore(asyncReducer, composeWithDevTools(
+  applyMiddleware(thunk),
+));
 
 export default store;
