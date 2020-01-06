54d39e7e18d60f0045bef012406b35ab849c93bf
oluwaseun bello <bellomsean@gmail.com>
2020-01-06 19:48:57 +0100

    finished landingpg sytling

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -2,27 +2,31 @@ import React from "react";
 import logo from "../logo.svg";
 import "./App.css";
 import { Link } from 'react-router-dom';
-
+import { Button } from 'react-bootstrap';
 
 class LandingPage extends React.Component {
+
+  goto = () => {
+    this.props.history.push('/doctorlist')
+  }
+
   render() {
     return (
       <div className="App">
         <div className="headTitle">
-          {" "}
-          <span>&#60;</span> Doctors
+          <span className='lessThan'><Link to='/'>&#60;</Link></span>Doctors
         </div>
         <header className="App-body">
           <div className="body">
             <input
               type="text"
-              className="input"
+              className="homepg-input"
               placeholder="Search for doctors"
-            />cd 
+            />
             <br />
             <br />
             <img src={logo} className="App-logo" alt="logo" />
-            <br />
+            <br /> <br />
             <h2 className="bodyTitle">search doctors</h2>
             <span>Search by directly clicking the doctors category below</span>
             <br />
@@ -31,31 +35,60 @@ class LandingPage extends React.Component {
         </header>
         <div>
           <div className='flex'>
-            <button className='cat'><Link to='/doctorlist'>All</Link></button>
-            <button className='cat'><Link to='/doctorlist'>Dermatologists</Link></button>
-            <button className='cat'><Link to='/doctorlist'>Endocrinologits</Link></button>
+            <Button className='cat' onClick={this.goto}>All</Button>
+            <Button className='cat' onClick={this.goto}>Dermatologists</Button>
+            <Button className='cat' onClick={this.goto}>Endocrinologits</Button>
           </div>
           <br />
           <div className='flex'>
-            <button className='cat'><Link to='/doctorlist'>Family Physician</Link></button>
-            <button className='cat'><Link to='/doctorlist'>Vent Doctor</Link></button>
-            <button className='cat'><Link to='/doctorlist'>Abecos</Link></button>
+            <Button className='cat' onClick={this.goto}>Family Physician</Button>
+            <Button className='cat' onClick={this.goto}>Vent Doctor</Button>
+            <Button className='cat' onClick={this.goto}>Abecos</Button>
           </div>
+          <br />
+          <div className='flex'>
+            <Button className='cat' onClick={this.goto}>Cardiac</Button>
+            <Button className='cat' onClick={this.goto}>Dermatologists</Button>
+            <Button className='cat' onClick={this.goto}>Endocrinologits</Button>
+          </div>
+          <br />
+          <div className='flex'>
+            <Button className='cat' onClick={this.goto}>Family Physician</Button>
+            <Button className='cat' onClick={this.goto}>Vent Doctor</Button>
+            <Button className='cat' onClick={this.goto}>Abecos</Button>
+          </div>
+          <br />
+          <div className='flex'>
+            <Button className='cat' onClick={this.goto}>Cardiac</Button>
+            <Button className='cat' onClick={this.goto}>Dermatologists</Button>
+            <Button className='cat' onClick={this.goto}>Endocrinologits</Button>
+          </div>
+          <br />
+          <div className='flex'>
+            <Button className='cat' onClick={this.goto}>Family Physician</Button>
+            <Button className='cat' onClick={this.goto}>Vent Doctor</Button>
+            <Button className='cat' onClick={this.goto}>Abecos</Button>
+          </div>
+          <br />
+          <span>copyright @bellom 2020</span>
+
         </div>
-        <ul>
-          <li>
-            <i className="material-icons">home</i>
-          </li>
-          <li>
-            <i className="material-icons">description</i>
-          </li>
-          <li>
-            <i className="material-icons">forum</i>
-          </li>
-          <li>
-            <i className="material-icons">notifications</i>
-          </li>
-        </ul>
+        <footer className='footer'>
+          <ul>
+            <li>
+              <i className="material-icons">home</i>
+            </li>
+            <li>
+              <i className="material-icons">description</i>
+            </li>
+            <li>
+              <i className="material-icons">forum</i>
+            </li>
+            <li>
+              <i className="material-icons">notifications</i>
+            </li>
+          </ul>
+        </footer>
       </div>
     );
   }
