ca8170ce534d0d574f6187a9ed8154efe6fc4278
oluwaseun bello <bellomsean@gmail.com>
2020-01-11 06:20:04 +0100

    passed test with gem shoulda_matchers

---
+++ spec/controllers/appointments_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe AppointmentsController, type: :controller do
+
+  # describe "GET #users" do
+  #   it { is_expected.to validate_presence_of :user_name}
+  # end
+
+end
---
+++ spec/models/appointment_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Appointment, type: :model do
+  describe "to validate appointment entities" do
+    it { is_expected.to validate_presence_of :date}
+    it { is_expected.to validate_presence_of :user_id}
+    it { is_expected.to validate_presence_of :doctor_id}
+    it { is_expected.to validate_presence_of :doctor_name}
+    it { is_expected.to validate_presence_of :user_name}
+  end
+end
