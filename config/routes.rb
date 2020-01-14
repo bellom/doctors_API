5e4e228382291e09e02aa41e83c9399147d222c3
oluwaseun bello <bellomsean@gmail.com>
2020-01-14 14:32:45 +0100

    fixing heroku issue

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
     resources :users
     resources :appointments
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
